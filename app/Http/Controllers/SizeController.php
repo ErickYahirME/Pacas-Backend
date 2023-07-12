d5aee57bacf4fcf36d95dc5135b05dc3547219bf
JorgeMateos13 <mateosjorge026@gmail.com>
2023-07-12 07:27:44 -0600

    Size and TypeClothe crud

--- app/Http/Controllers/SizeController.php
+++ app/Http/Controllers/SizeController.php
@@ -2,9 +2,48 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\size;
 use Illuminate\Http\Request;
 
 class SizeController extends Controller
 {
-    //
+    public function getSize()
+    {
+        Return response()->json(size::all(), 200);
+    }
+
+    public function getSizeById($id)
+    {
+        $size = size::find($id);
+        if(is_null($size)){
+            return response()->json(['message'=>'Size not found'], 404);
+        }
+        return response()->json($size::find($id), 200);
+    }
+
+    public function addSize(Request $request)
+    {
+        $size = size::create($request->all());
+        return response($size, 201);
+    }
+
+    public function updateSize(Request $request, $id)
+    {
+        $size = size::find($id);
+        if(is_null($size)){
+            return response()-> json(['message'=>'Size not found'], 404);
+        }
+        $size->update($request->all());
+        return response($size, 200);
+    }
+
+    public function deleteSize($id)
+    {
+        $size = size::find($id);
+        if(is_null($size)){
+            return response()->json(['message'=> 'Size not found'], 404);
+        }
+        $size->delete();
+        return response()->json(['message'=> 'Size deleted', null], 204);
+    }
 }
--- app/Http/Controllers/TypeClotheController.php
+++ app/Http/Controllers/TypeClotheController.php
@@ -2,9 +2,48 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\typeClothe;
 use Illuminate\Http\Request;
 
 class TypeClotheController extends Controller
 {
-    //
+    public function getTypeClothe()
+    {
+        Return response()->json(tClothe::all(), 200);
+    }
+
+    public function getTypeClotheById($id)
+    {
+        $tClothe = tClothe::find($id);
+        if(is_null($tClothe)){
+            return response()->json(['message'=>'Type of clothe not found'], 404);
+        }
+        return response()->json($tClothe::find($id), 200);
+    }
+
+    public function addTypeClothe(Request $request)
+    {
+        $tClothe = tClothe::create($request->all());
+        return response($tClothe, 201);
+    }
+
+    public function updateTypeClothe(Request $request, $id)
+    {
+        $tClothe = tClothe::find($id);
+        if(is_null($tClothe)){
+            return response()-> json(['message'=>'Type of clothe not found'], 404);
+        }
+        $tClothe->update($request->all());
+        return response($tClothe, 200);
+    }
+
+    public function deleteTypeClothe($id)
+    {
+        $tClothe = tClothe::find($id);
+        if(is_null($tClothe)){
+            return response()->json(['message'=> 'Type of clothe not found'], 404);
+        }
+        $tClothe->delete();
+        return response()->json(['message'=> 'Type of clothe deleted', null], 204);
+    }
 }
